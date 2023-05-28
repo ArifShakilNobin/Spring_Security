612cf3c3af833c4520e38d197e9ba555a367a17c
Arif.Shakil <arif.shakil@synesisit.com.bd>
2023-05-28 10:44:20 +0600

    first commit

---
+++ src/main/java/SpringSecurity/config/SecurityConfig.java
@@ -0,0 +1,19 @@
+package SpringSecurity.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http){
+        return http
+                .addFilter(, UsernamePasswordAuthenticationFilter.class)
+                .build();
+
+    }
+}
---
+++ src/main/java/SpringSecurity/config/security/filters/CustomAuthenticationFilter.java
@@ -0,0 +1,13 @@
+package SpringSecurity.config.security.filters;
+
+
+import jakarta.servlet.*;
+
+import java.io.IOException;
+
+public class CustomAuthenticationFilter implements Filter {
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        
+    }
+}
